81457521466ea9631bfbe335a2c0f1087fdf4f40
NgocThinh <nguyen06@uvic.ca>
2018-07-14 07:37:47 -0700

    Moved 'fortube cookie' functionlityinto module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,11 +9,6 @@ app.use(express.static(__dirname + '/public'));
 var handlebars = require('express3-handlebars') .create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-    ];
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
@@ -24,8 +20,7 @@ app.get('/', function(req, res){
 app.get('/about',function(req, res){
     // res.type('text/plain');
     // res.send('About Meadowlark travel');
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 })
 //custom 404 page
 app.use(function(req, res, next){
